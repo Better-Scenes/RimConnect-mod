54108d5b8beead606a44c64f927959b90cea3fe1
Russell Long <russelllongj@gmail.com>
2019-08-21 16:26:01 +1000

    Add simple timer that spawn drop pods

---
+++ Source/RimConnection/ServerRequester.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Timers;
+
+namespace RimConnection
+{
+    class ServerRequester
+    {
+        private static System.Timers.Timer timer;
+
+        public ServerRequester(int intervalMs)
+        {
+            timer = new System.Timers.Timer(intervalMs);
+            timer.Elapsed += onTimerTimeout;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+        }
+
+        private static void onTimerTimeout(Object source, ElapsedEventArgs e)
+        {
+            DropPodManager.createDrop();
+        }
+
+    }
+}
