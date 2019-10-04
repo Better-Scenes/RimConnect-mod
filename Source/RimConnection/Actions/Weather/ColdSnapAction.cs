be12faf3688c9c10270fd676988b4ca20e4eb7d8
Russell Long <russelllongj@gmail.com>
2019-10-04 22:19:51 +1000

    Hopefully added working cold snap

---
+++ Source/RimConnection/Actions/Weather/ColdSnapAction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using RimWorld;
+using Verse;
+
+namespace RimConnection
+{
+    class ColdSnapAction : Action
+    {
+        public ColdSnapAction()
+        {
+            this.name = "Cold Snap";
+            this.description = "Better rug up!";
+        }
+
+        public override void execute(int amount)
+        {
+            var currentMap = Find.CurrentMap;
+
+            var parms = StorytellerUtility.DefaultParmsNow(IncidentCategoryDefOf.ThreatBig, currentMap);
+            parms.forced = true;
+            new IncidentWorker_ColdSnap().TryExecute(parms);
+        }
+    }
+}
