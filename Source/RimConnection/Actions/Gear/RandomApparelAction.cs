f3d3afa5f34523cc890c5fc277d6464a382cd9cd
Russell Long <russelllongj@gmail.com>
2019-10-05 11:47:03 +1000

    Really add the gear actions

---
+++ Source/RimConnection/Actions/Gear/RandomApparelAction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using RimWorld;
+using Verse;
+
+namespace RimConnection
+{
+    class RandomApparelAction : Action
+    {
+        public RandomApparelAction()
+        {
+            this.name = "Random Apparel";
+            this.description = "Clothing, armour, it's all the same to me";
+            this.canSpawnMultiple = true;
+            this.category = "Gear";
+        }
+
+        public override void execute(int amount)
+        {
+            var apparelThingDefs = DefDatabase<ThingDef>.AllDefs.Where(def => { return def.IsApparel; });
+            
+            DropPodManager.createDrop(apparelThingDefs.RandomElement(), amount, name, description);
+        }
+    }
+}
---
+++ Source/RimConnection/Actions/Gear/ShieldBeltAction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using RimWorld;
+
+namespace RimConnection
+{
+    class ShieldBeltAction : Action
+    {
+        public ShieldBeltAction()
+        {
+            this.name = "Shield Belt";
+            this.description = "The only way in or out is a tiny hole at the top";
+            this.canSpawnMultiple = true;
+            this.category = "Gear";
+        }
+
+        public override void execute(int amount)
+        {
+            DropPodManager.createDrop(ThingDefOf.Apparel_ShieldBelt, amount, name, description);
+        }
+    }
+}
