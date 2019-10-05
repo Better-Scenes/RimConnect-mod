ed0b36c276d928fa8529ae7cc8c695bfc1f2ff0c
Russell Long <russelllongj@gmail.com>
2019-10-05 14:55:53 +1000

    Add batch for building, add preview and worksgop ID

---
+++ quicktest.bat
@@ -0,0 +1,8 @@
+rmdir /S /Q "D:\games\Steam\steamapps\common\RimWorld\Mods\RimConnect"
+mkdir "D:\games\Steam\steamapps\common\RimWorld\Mods\RimConnect"
+
+robocopy "%cd%/Assemblies" "D:\games\Steam\steamapps\common\RimWorld\Mods\RimConnect\Assemblies" /s
+robocopy "%cd%/About" "D:\games\Steam\steamapps\common\RimWorld\Mods\RimConnect\About" /s
+robocopy "%cd%/Languages" "D:\games\Steam\steamapps\common\RimWorld\Mods\RimConnect\Languages" /s
+
+REM start D:\games\Steam\steamapps\common\RimWorld\RimWorldWin64.exe -quicktest
