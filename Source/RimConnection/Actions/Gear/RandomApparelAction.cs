5003228ae3a5b6f78cceb1e522df0d03b84b9fe3
Russell Long <russelllongj@gmail.com>
2019-10-11 16:33:41 +1100

    Add randomness to item quality

--- Source/RimConnection/Actions/Gear/RandomApparelAction.cs
+++ Source/RimConnection/Actions/Gear/RandomApparelAction.cs
@@ -20,11 +20,26 @@ namespace RimConnection
 
         public override void execute(int amount)
         {
+            Random random = new Random();
+
             var apparelThingDefs = DefDatabase<ThingDef>.AllDefs.Where(def => { return def.IsApparel; });
 
             var randomApparelDefList = apparelThingDefs.ToList().TakeRandom(amount);
 
-            var apparelThings = randomApparelDefList.Select(apparelDef => { return ThingMaker.MakeThing(apparelDef); });
+            var apparelThings = randomApparelDefList.Select(apparelDef => {
+                var stuffForThing = GenStuff.RandomStuffFor(apparelDef);
+                var thing = ThingMaker.MakeThing(apparelDef, stuffForThing);
+
+                var thingQualityComp = thing.TryGetComp<CompQuality>();
+                if (thingQualityComp != null)
+                {
+                    Array qualityValues = Enum.GetValues(typeof(QualityCategory));
+                    QualityCategory randomQuality = (QualityCategory)qualityValues.GetValue(random.Next(qualityValues.Length));
+
+                    thingQualityComp.SetQuality(randomQuality, ArtGenerationContext.Colony);
+                }
+                return thing;
+            });
             
             DropPodManager.createDropOfThings(apparelThings.ToList(), name, description);
         }
