7fe2955d1fcf6c1ca674a5825fba5bd3e3eddaa6
Russell Long <russelllongj@gmail.com>
2019-10-11 15:14:46 +1100

    Remove logging

--- Source/RimConnection/Actions/ActionList.cs
+++ Source/RimConnection/Actions/ActionList.cs
@@ -25,7 +25,6 @@ namespace RimConnection
 
         public static ValidCommandList ActionListToApi()
         {
-            actionList().ForEach(action => Log.Message(action.name));
             ValidCommandList validCommandList = new ValidCommandList();
             validCommandList.validCommands = actionList().Select((action, index) => action.toApiCall(index)).ToList();
             return validCommandList;
