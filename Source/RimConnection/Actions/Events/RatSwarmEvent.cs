06d3fd68fdccb9e97c94a475a7d2b0c78e82a184
Russell Long <russelllongj@gmail.com>
2019-11-06 17:50:46 +1100

    Make rats spread apart properly when they spawn in

--- Source/RimConnection/Actions/Events/RatSwarmEvent.cs
+++ Source/RimConnection/Actions/Events/RatSwarmEvent.cs
@@ -20,20 +20,24 @@ namespace RimConnection
         public override void execute(int amount)
         {
             var currentMap = Find.CurrentMap;
-            var entryCell = CellFinder.RandomEdgeCell(currentMap);
+            var rootEntryCell = CellFinder.RandomEdgeCell(currentMap);
             var rat = DefDatabase<PawnKindDef>.GetNamed("Rat");
+
+            Predicate<IntVec3> validator = (IntVec3 c) => c.InBounds(currentMap) && c.Standable(currentMap);
 
             var colonistCount = Find.ColonistBar.GetColonistsInOrder().Count;
             var ratCount = 2 * colonistCount;
 
             for (int i = 0; i < ratCount; i++)
             {
+                IntVec3 entryCell;
+                CellFinder.TryFindRandomCellNear(rootEntryCell, currentMap, 5, validator, out entryCell);
                 var newRat = Verse.PawnGenerator.GeneratePawn(rat);
                 GenSpawn.Spawn(newRat, entryCell, currentMap);
                 newRat.mindState.mentalStateHandler.TryStartMentalState(MentalStateDefOf.Manhunter);
             }
            
-            Find.LetterStack.ReceiveLetter("Twitch Event", "A swarm of aggresive rats has appeared. You might need to hide", LetterDefOf.NegativeEvent, new LookTargets(entryCell, currentMap));
+            Find.LetterStack.ReceiveLetter("Twitch Event", "A swarm of aggresive rats has appeared. You might need to hide", LetterDefOf.NegativeEvent, new LookTargets(rootEntryCell, currentMap));
         }
     }
 }
