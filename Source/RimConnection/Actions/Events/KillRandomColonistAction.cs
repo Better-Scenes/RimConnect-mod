641e8356135636a5c5fb4961b4d68f75c0e4861c
Russell Long <russelllongj@gmail.com>
2019-11-11 20:42:01 +1100

    Make colonists have heart attacks instead of outright dying

--- Source/RimConnection/Actions/Events/KillRandomColonistAction.cs
+++ Source/RimConnection/Actions/Events/KillRandomColonistAction.cs
@@ -12,38 +12,25 @@ namespace RimConnection
     {
         public KillRandomColonistAction()
         {
-            this.name = "Kill random colonist";
-            this.description = "You must be awfully cruel to select something like this";
+            this.name = "Sheer Heart Attack";
+            this.description = "Sheeeeeeer Cardiac!";
             this.category = "Event";
         }
 
         public override void execute(int amount)
         {
-            var colonists = Find.ColonistBar.GetColonistsInOrder();
+            var heartAttackDef = DefDatabase<HediffDef>.GetNamed("HeartAttack");
+
+            var colonists = Find.ColonistBar.GetColonistsInOrder().Where(colonist => !colonist.Dead);
 
             var randomColonists = colonists.InRandomOrder().Take(amount);
-            var colonistNames = new List<string>();
             foreach (var colonist in randomColonists)
             {
-                colonistNames.Add(colonist.Name.ToStringShort);
-                colonist.Kill(new DamageInfo(DamageDefOf.Crush, 999999));
+                var heartAttackHeDiff = HediffMaker.MakeHediff(heartAttackDef, colonist);
+                colonist.health.AddHediff(heartAttackHeDiff);
             }
 
-            string colonistsAsJoinedString;
-            if (colonistNames.Count == 1)
-            {
-                colonistsAsJoinedString = colonistNames[0];
-            }
-            else
-            {
-                var allColonistsExceptOneNames = colonistNames.Take(colonistNames.Count - 1);
-                var lastColonistName = colonistNames[colonistNames.Count - 1];
-
-                colonistsAsJoinedString = String.Join(", ", allColonistsExceptOneNames.ToArray());
-                colonistsAsJoinedString += $", and {lastColonistName}";
-            }
-
-            var label = $"Your twitch viewers decided it was better if {colonistsAsJoinedString} weren't around anymore";
+            var label = $"Your twitch viewers decided it was better if some of your colonists had heart attacks. I'm so sorry....";
             Find.LetterStack.ReceiveLetter("Twitch Event", label, LetterDefOf.NegativeEvent);
         }
     }
