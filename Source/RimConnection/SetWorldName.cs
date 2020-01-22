72667e6eddf8561ac0fa59fe14704768b081f585
Russell Long <russelllongj@gmail.com>
2020-01-22 20:24:34 +1100

    Add a component to print the world name for testing store

---
+++ Source/RimConnection/SetWorldName.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Verse;
+
+namespace RimConnection
+{
+    class SetWorldName : GameComponent
+    {
+        public SetWorldName(Game game)
+        {
+        }
+
+        public SetWorldName()
+        {
+            Log.Message($"World name is {Find.World.info.name}");
+        }
+
+        public override void FinalizeInit()
+        {
+            Log.Message($"World name is {Find.World.info.name}");
+        }
+    }
+}
