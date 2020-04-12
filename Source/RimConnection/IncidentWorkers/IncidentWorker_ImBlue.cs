4fde7e06214b18611f0502222ddf023686031d9a
Russell Long <russelllongj@gmail.com>
2020-04-13 09:46:27 +1000

    Start creating the I'm Blue incident worker

---
+++ Source/RimConnection/IncidentWorkers/IncidentWorker_ImBlue.cs
@@ -0,0 +1,20 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+
+// Copied from the Rimworld namespace, but made public
+public class IncidentWorker_ImBlue : IncidentWorker
+{
+    protected override bool CanFireNowSub(IncidentParms parms)
+    {
+        return true;
+    }
+
+    protected override bool TryExecuteWorker(IncidentParms parms)
+    {
+        Pawn randomColonist = Find.ColonistBar.GetColonistsInOrder().RandomElement();
+
+        randomColonist.story.SkinColor = new Color(73, 66, 255);
+        return true;
+    }
+}
