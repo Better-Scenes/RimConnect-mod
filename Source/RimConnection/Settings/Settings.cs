7ab187f578141bbcfae15ddffeddde246f0824ba
hodlhodl <don@dmsgmail.com>
2020-06-08 16:08:06 -0700

    feat: ability to alter base url of rimconnect api

--- Source/RimConnection/Settings/Settings.cs
+++ Source/RimConnection/Settings/Settings.cs
@@ -11,9 +11,7 @@ namespace RimConnection
     {
         public static string[] validCommands;
 
-        //public static string BASE_URL = "https://rimconnect-dev.herokuapp.com/";
         public static string BASE_URL = "http://rimconnect-backend.herokuapp.com/";
-        //public static string BASE_URL = "http://localhost:8080/";
 
         public static string BaseURL()
         {
