7811d1a7f961ebf3e0cc14647b9b6b7f8bf3de9f
hodlhodl <don@dmsgmail.com>
2020-06-08 18:31:28 -0700

    feat: New random category storyteller voting comp

---
+++ Source/RimConnection/Voting/StorytellerComps/StorytellerCompProperties_VotingRandomMain.cs
@@ -0,0 +1,24 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace RimConnection.Voting.StorytellerComps
+{
+    public class StorytellerCompProperties_VotingRandomMain : StorytellerCompProperties
+    {
+        public StorytellerCompProperties_VotingRandomMain()
+        {
+            this.compClass = typeof(StorytellerComp_VotingRandomMain);
+        }
+
+        public float mtbDays;
+
+        public List<IncidentCategoryEntry> categoryWeights = new List<IncidentCategoryEntry>();
+
+        public float maxThreatBigIntervalDays = 99999f;
+
+        public FloatRange randomPointsFactorRange = new FloatRange(0.5f, 1.5f);
+
+        public bool skipThreatBigIfRaidBeacon;
+    }
+}
---
+++ Source/RimConnection/Voting/StorytellerComps/StorytellerComp_VotingRandomMain.cs
@@ -0,0 +1,87 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace RimConnection.Voting.StorytellerComps
+{
+    public class StorytellerComp_VotingRandomMain : StorytellerComp
+    {
+        protected StorytellerCompProperties_VotingRandomMain Props
+        {
+            get
+            {
+                return (StorytellerCompProperties_VotingRandomMain)this.props;
+            }
+        }
+
+        public override IEnumerable<FiringIncident> MakeIntervalIncidents(IIncidentTarget target)
+        {
+            List<IncidentDef> incidentDefs = new List<IncidentDef>();
+            List<IncidentCategoryDef> categorySkips = new List<IncidentCategoryDef>();
+
+            if (Rand.MTBEventOccurs(this.Props.mtbDays, 60000f, 1000f) || RimConnectSettings.forceRandom)
+            {
+                bool failed = false;
+
+                while (incidentDefs.Count < 4 && !failed)
+                {
+                    IncidentCategoryDef incidentCategoryDef = this.ChooseRandomCategory(target, categorySkips);
+                    IncidentParms parms = this.GenerateParms(incidentCategoryDef, target);
+
+                    IEnumerable<IncidentDef> usuableIncidentDefs = UsableIncidentsInCategory(incidentCategoryDef, parms)
+                        .Where((x) => !incidentDefs.Contains(x));
+
+                    if (usuableIncidentDefs.Count() < 1)
+                    {
+                        categorySkips.Add(incidentCategoryDef);
+                    }
+
+                    if (usuableIncidentDefs.TryRandomElementByWeight(new Func<IncidentDef, float>(base.IncidentChanceFinal), out IncidentDef incidentDef))
+                    {
+                        incidentDefs.Add(incidentDef);
+                    }
+                    else
+                    {
+                        failed = true;
+                    }
+                }
+
+                foreach (IncidentDef incidentDef in incidentDefs)
+                {
+                    Log.Message($"Generated incident {incidentDef.LabelCap}");
+                }
+            }
+
+            yield break;
+        }
+
+        private IncidentCategoryDef ChooseRandomCategory(IIncidentTarget target, List<IncidentCategoryDef> skipCategories)
+        {
+            if (!skipCategories.Contains(IncidentCategoryDefOf.ThreatBig))
+            {
+                int num = Find.TickManager.TicksGame - target.StoryState.LastThreatBigTick;
+                if (target.StoryState.LastThreatBigTick >= 0 && (float)num > 60000f * this.Props.maxThreatBigIntervalDays)
+                {
+                    return IncidentCategoryDefOf.ThreatBig;
+                }
+            }
+            return (from cw in this.Props.categoryWeights
+                    where !skipCategories.Contains(cw.category)
+                    select cw).RandomElementByWeight((IncidentCategoryEntry cw) => cw.weight).category;
+        }
+
+        public override IncidentParms GenerateParms(IncidentCategoryDef incCat, IIncidentTarget target)
+        {
+            IncidentParms incidentParms = StorytellerUtility.DefaultParmsNow(incCat, target);
+            if (incidentParms.points >= 0f)
+            {
+                incidentParms.points *= this.Props.randomPointsFactorRange.RandomInRange;
+            }
+            return incidentParms;
+        }
+    }
+}
